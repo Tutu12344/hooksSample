f941e2bf07738f77c958b3cf0943a83acca08371
Tutu12344 <padde.cross@gmail.com>
2020-09-26 16:18:08 +0900

    usestate learn(配列)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Basic1 from "./component/Basic1";
+import Basic2 from "./component/Basic2";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic1 name="hook" />
+        <Basic2 />
       </header>
     </div>
   );
