d6b9a818763e0a36aaa96542dcb3c585592417aa
Tutu12344 <padde.cross@gmail.com>
2020-09-26 15:43:27 +0900

    event

--- src/component/basic1.js
+++ src/component/basic1.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const basic1 = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
   return (
     <div>
       <h1>{props.name}</h1>
+      <button onClick={clickHandler}>Click</button>
     </div>
   );
 };
