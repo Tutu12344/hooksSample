b10e0a3be90f7db07f97b02d29c17a48cffed0b2
Tutu12344 <padde.cross@gmail.com>
2020-09-26 21:07:58 +0900

    useReducer*useContext learn

--- src/App.js
+++ src/App.js
@@ -9,9 +9,30 @@ import B from "./component/B";
 // import TimerContainer from "./component/TimerContainer";
 import ApiFetch from "./component/ApiFetch";
 import BasicReducer from "./component/BasicReducer";
+import CompB from "./component/CompB";
+
+import { useReducer } from "react";
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+  switch (action) {
+    case "add_1":
+      return currentState + 1;
+    case "multiple_3":
+      return currentState * 3;
+    case "reset":
+      return initialState;
+    default:
+      return currentState;
+  }
+};
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <AppContext.Provider value={"value from App.js"}>
+    <AppContext.Provider
+      value={{ countProvided: count, dispatchProvided: dispatch }}
+    >
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +41,9 @@ function App() {
           {/* <TimerContainer /> */}
           {/* <ApiFetch />
           <B /> */}
-          <BasicReducer />
+          {/* <BasicReducer /> */}
+          Count {count}
+          <CompB />
         </header>
       </div>
     </AppContext.Provider>
