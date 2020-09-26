18bf9c5d1cd38ffd1d45c3b80b1bbc2ded625dc3
Tutu12344 <padde.cross@gmail.com>
2020-09-26 16:06:46 +0900

    usestate learn(object)

--- src/component/basic1.js
+++ src/component/basic1.js
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 
-const Basic1 = (props) => {
-  //   const clickHandler = () => {
-  //     console.log("clicked");
-  //   };
-
-  const [count, setCount] = useState(0);
+const Basic1 = () => {
+  const [product, setProducts] = useState({ name: "", price: "" });
   return (
     <div>
-      <h1>{props.name}</h1>
-      <button
-        onClick={() => {
-          setCount((prevCount) => prevCount + 1);
-          setCount((prevCount) => prevCount + 1);
-        }}
-      >
-        Count{count}
-      </button>
-      {console.log(setCount)}
+      <form>
+        {/* ...をつけることでobjectの中身を分解して展開し、nameだけの値を変えることができる（これをおこわないとpriceが失われてしまう） */}
+        <input
+          type="text"
+          value={product.name}
+          onChange={(event) =>
+            setProducts({ ...product, name: event.target.value })
+          }
+        />
+        <input
+          type="text"
+          value={product.price}
+          onChange={(event) =>
+            setProducts({ ...product, price: event.target.value })
+          }
+        />
+      </form>
+      {console.log(product.name)}
+      {console.log("aaa" + product.price)}
     </div>
   );
 };
