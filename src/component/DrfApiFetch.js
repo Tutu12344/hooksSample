e5c924745e61426954ca901dadea882941cc5c0f
Tutu12344 <padde.cross@gmail.com>
2020-09-27 17:42:22 +0900

    delete spa learn

--- src/component/DrfApiFetch.js
+++ src/component/DrfApiFetch.js
@@ -29,7 +29,7 @@ const DrfApiFetch = () => {
       });
   };
 
-  const deleteTask = () => {
+  const deleteTask = (id) => {
     axios
       .delete(`http://127.0.0.1:8000/api/tasks/${id}/`, {
         headers: {
@@ -37,7 +37,8 @@ const DrfApiFetch = () => {
         },
       })
       .then((res) => {
-        console.log(res);
+        setTasks(tasks.filter((task) => task.id !== id));
+        setSelectedTask([]);
       });
   };
   return (
@@ -47,6 +48,9 @@ const DrfApiFetch = () => {
           <li key={task.id}>
             {task.title}
             {task.id}
+            <button onClick={() => deleteTask(task.id)}>
+              <i className="fas fa-trash-alt"></i>
+            </button>
           </li>
         ))}
       </ul>
