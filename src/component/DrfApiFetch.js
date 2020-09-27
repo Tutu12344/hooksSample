1ab4ca1b0b3f7d539ba842c3f8c8dbd37ada49b6
Tutu12344 <padde.cross@gmail.com>
2020-09-27 17:56:14 +0900

    create spa learn

--- src/component/DrfApiFetch.js
+++ src/component/DrfApiFetch.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const DrfApiFetch = () => {
   const [tasks, setTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState([]);
+  const [editedTask, setEditedTask] = useState({ id: "", title: "" });
   const [id, setId] = useState(1);
   useEffect(() => {
     axios
@@ -17,6 +18,20 @@ const DrfApiFetch = () => {
       });
   }, []);
 
+  const newTask = (task) => {
+    const data = {
+      title: task.title,
+    };
+    axios
+      .post(`http://127.0.0.1:8000/api/tasks/`, data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token 8d8af9d040ceae1ff5e66f65c7fc8ae1d93722db",
+        },
+      })
+      .then((res) => setTasks([...tasks, res.data]));
+    console.log(editedTask);
+  };
   const getTask = () => {
     axios
       .get(`http://127.0.0.1:8000/api/tasks/${id}/`, {
@@ -40,6 +55,12 @@ const DrfApiFetch = () => {
         setTasks(tasks.filter((task) => task.id !== id));
         setSelectedTask([]);
       });
+  };
+
+  const handleInputChange = () => (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    setEditedTask({ ...editedTask, [name]: value });
   };
   return (
     <div>
@@ -72,6 +93,16 @@ const DrfApiFetch = () => {
         {selectedTask.title}
         {selectedTask.id}
       </h3>
+
+      <input
+        type="text"
+        name="title"
+        value={editedTask.title}
+        onChange={handleInputChange()}
+        placeholder="NewTask?"
+        required
+      />
+      <button onClick={() => newTask(editedTask)}>Create</button>
     </div>
   );
 };
